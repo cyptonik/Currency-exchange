07dcc318ebf9ce792879d33580c0feb3569d3feb
Cypopik <enixydex15@gmail.com>
2026-03-29 19:14:56 +0300

    Replace try\catch block with Optional methods .orElse

--- app/src/main/java/service/ExchangeService.java
+++ app/src/main/java/service/ExchangeService.java
@@ -9,6 +9,7 @@ import model.ExchangeRate;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Optional;
 
 public class ExchangeService {
     private final ExchangeRatesDao erDao;
@@ -31,56 +32,50 @@ public class ExchangeService {
         Currency currencyFrom = currencyDao.getCurrencyByCode(from);
         Currency currencyTo = currencyDao.getCurrencyByCode(to);
 
-        try {
-            return convertFromDirect(from, to, amount);
-        } catch (NotFoundException e1) {
-            try {
-                return convertFromInverse(currencyFrom, currencyTo, amount);
-            } catch (NotFoundException e2) {
-                try {
-                    return convertFromUsd(currencyFrom, currencyTo, amount);
-                } catch (NotFoundException e3) {
-                    throw new NotFoundException("Couldn't find exchange rates");
-                }
-            }
-        }
+        return convertFromDirect(from, to, amount)
+                .orElseGet(() -> convertFromInverse(currencyFrom, currencyTo, amount)
+                        .orElseGet(() -> convertFromInverse(currencyFrom, currencyTo, amount)
+                                .orElseThrow(() -> new NotFoundException("Couldn't find exchange rates"))));
+
     }
 
-    private ExchangeDto convertFromDirect(String from, String to, BigDecimal amount) {
+    private Optional<ExchangeDto> convertFromDirect(String from, String to, BigDecimal amount) {
         ExchangeRate exchangeRate = erDao.getExchangeRateByCode(from + to);
-        return new ExchangeDto(
+        return Optional.of(new ExchangeDto(
                 exchangeRate.getBaseCurrency(),
                 exchangeRate.getTargetCurrency(),
                 exchangeRate.getRate(),
                 amount,
-                amount.multiply(exchangeRate.getRate()));
+                amount.multiply(exchangeRate.getRate()))
+        );
     }
 
-    private ExchangeDto convertFromInverse(Currency currencyFrom, Currency currencyTo, BigDecimal amount) {
+    private Optional<ExchangeDto> convertFromInverse(Currency currencyFrom, Currency currencyTo, BigDecimal amount) {
         ExchangeRate inverseExchangeRate = erDao.getExchangeRateByCode(currencyTo.getCode() + currencyFrom.getCode());
         BigDecimal rate = BigDecimal.ONE.divide(inverseExchangeRate.getRate(), 10, roundingMode);
 
-        return new ExchangeDto(
+        return Optional.of(new ExchangeDto(
                 currencyFrom,
                 currencyTo,
                 rate,
                 amount,
-                amount.multiply(rate));
+                amount.multiply(rate))
+        );
     }
 
-    private ExchangeDto convertFromUsd(Currency currencyFrom, Currency currencyTo, BigDecimal amount) {
+    private Optional<ExchangeDto> convertFromUsd(Currency currencyFrom, Currency currencyTo, BigDecimal amount) {
         BigDecimal usdToFromRate = getUsdRate(currencyFrom);
         BigDecimal usdToToRate = getUsdRate(currencyTo);
 
         BigDecimal rate = usdToToRate.divide(usdToFromRate, 10, roundingMode);
 
-        return new ExchangeDto(
+        return Optional.of(new ExchangeDto(
                 currencyFrom,
                 currencyTo,
                 rate,
                 amount,
                 amount.multiply(rate)
-        );
+        ));
     }
 
     private BigDecimal getUsdRate(Currency currency) {
@@ -90,7 +85,7 @@ public class ExchangeService {
             return erDao.getExchangeRateByCode("USD" + currency.getCode()).getRate();
         } catch (NotFoundException e) {
             BigDecimal inverse = erDao.getExchangeRateByCode(currency.getCode() + "USD").getRate();
-            return BigDecimal.ONE.divide(inverse, 6, RoundingMode.HALF_UP);
+            return BigDecimal.ONE.divide(inverse, 6, roundingMode);
         }
     }
 }
