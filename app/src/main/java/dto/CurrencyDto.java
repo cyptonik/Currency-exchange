2f842cddf31422cb1dcc7ca4b4470fc46f6092e1
Cypopik <enixydex15@gmail.com>
2026-03-30 03:11:49 +0300

    Created CurrencyDto and ExchangeRateDto

---
+++ app/src/main/java/dto/CurrencyDto.java
@@ -0,0 +1,31 @@
+package dto;
+
+public class CurrencyDto {
+    private final int id;
+    private final String code;
+    private final String fullName;
+    private final String sign;
+
+    public CurrencyDto(int id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+}
---
+++ app/src/main/java/dto/ExchangeRateDto.java
@@ -0,0 +1,36 @@
+package dto;
+
+import model.Currency;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ExchangeRateDto {
+    private final int id;
+    private final CurrencyDto baseCurrency;
+    private final CurrencyDto targetCurrency;
+    private final BigDecimal rate;
+
+    public ExchangeRateDto(int id, CurrencyDto baseCurrency, CurrencyDto targetCurrency, BigDecimal rate) {
+        this.id = id;
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public CurrencyDto getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public CurrencyDto getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
